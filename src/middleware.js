2914c6796ec31b08d837c556e9e6680c7b035cce
tristan <tristanriggi@gmail.com>
2024-11-27 01:25:09 -0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -6,7 +6,7 @@ export const config = {
 
 export function middleware(request) {
   const requestHeaders = new Headers(request.headers);
-  requestHeaders.set("x-createxyz-project-id", "99c8c6cd-8b50-475d-8488-859da778bfe0");
+  requestHeaders.set("x-createxyz-project-id", process.env.NEXT_PUBLIC_PROJECT_ID);
 
   request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
 
